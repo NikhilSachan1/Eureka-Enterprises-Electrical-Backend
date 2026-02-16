a898d861e28d41d31f3efa8a0d3d16e675d69ade
akhilcoditas <akhil.sachan@coditas.com>
2026-02-16 08:27:52 +0530

    Issue 28- resolved CODA

--- backend/src/modules/user-roles/constants/user-role.constants.ts
+++ backend/src/modules/user-roles/constants/user-role.constants.ts
@@ -1,3 +1,8 @@
 export const USER_ROLE_FIELD_NAMES = {
   USER_ROLE: 'User Role',
 } as const;
+
+export const USER_ROLE_ERRORS = {
+  ROLE_NOT_FOUND: (roleId: string) => `Role with ID ${roleId} not found`,
+  USER_NOT_FOUND: 'User not found',
+} as const;
